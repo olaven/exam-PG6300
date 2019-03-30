bfdc08b5bccb7af687feb3e003f9792f0d01ba7e
olaven <olavsundfoer@gmail.com>
2019-03-30 19:17:18 +0100

    footer also renders differnetly based on login

--- src/client/layout/footer.jsx
+++ src/client/layout/footer.jsx
@@ -3,8 +3,17 @@ import React from "react";
 export default class Layout extends React.Component {
 
     render() {
+
+        const loggedIn = this.props.username !== null;
+
         return <div style={style}>
             <h1>This is the footer</h1>
+            <p>
+                Thanks for using this site, {loggedIn? 
+                    this.props.username:
+                    "you great person"
+                }
+            </p>
         </div>
     }
 }
