348d7ab7aa33d904242757a729a5f0a8270e2988
olaven <olavsundfoer@gmail.com>
2019-04-09 22:43:18 +0200

    yet more work on GUI-tests. Not working

--- src/client/notFound.jsx
+++ src/client/notFound.jsx
@@ -4,10 +4,16 @@ import Layout from "./layout/layout";
 
 export class NotFound extends React.Component {
 
+    click = () => {
+
+        console.log("clicked");
+    }
+
     render() {
+        
         return <div>
             <h1>This page was not found..</h1>
-            <Link to={"/"} id="notFound-link">
+            <Link to={"/"} id="notFound-link" onClick={this.click}>
                 <h2>Go home.</h2>
             </Link>
         </div>
--- tests/client/notFound.test.js
+++ tests/client/notFound.test.js
@@ -1,5 +1,5 @@
 const React = require("react");
-const { MemoryRouter } = require("react-router-dom");
+const { MemoryRouter, Link } = require("react-router-dom");
 const { mount, shallow } = require("enzyme");
 const request = require("supertest");
 
@@ -9,48 +9,62 @@ const { app } = require("../../src/server/app");
 const { asyncCheckCondition, overrideFetchWithAgent } = require("./mytest-utils");
 
 
+const basicWrapper = (entry) => mount(
+	<MemoryRouter>
+		<NotFound initialEntries={[entry]}/>
+	</MemoryRouter>
+);
+
+const withHistory = (page) => {
+
+	const history = {push: (h) => {page=h;}};
+	return mount(
+		<MemoryRouter>
+			<NotFound history={history} />
+		</MemoryRouter>
+	);
+};
+
+const homeWithEntry = (entry) => mount(
+	<MemoryRouter initialEntries={[entry]}>
+		<Home />
+	</MemoryRouter>
+);
+
+
+
 describe("the not-found page.", () => {
 
-    let wrapper; 
+	it("has link to home", () => {
+        
+		const wrapper = basicWrapper();
+		const link = wrapper.find("#notFound-link").get(0);
+		expect(link).toBeDefined();
+		expect(wrapper.text()).toContain("Go home");
+	});
 
-    beforeEach(() => {
-
-        wrapper = mount(<MemoryRouter>
-            <NotFound />
-        </MemoryRouter>);
-    });
-
-    it("links back to home", () => {
-
-        const link = wrapper.find("#notFound-link").get(0);
-        expect(link).toBeDefined();
-        expect(wrapper.text()).toContain("Go home");
-    });
-
-    it("brings user home on click", async () => {
+	it("brings user home on click", async () => {
 
 		let page = null;
-		const history = {push: (h) => {page=h}};
-		wrapper = mount(
-			<MemoryRouter initialEntries={["/not-valid"]}>
-				<NotFound history={history} />
-			</MemoryRouter>
-		);
-
-		const link = wrapper.find("#notFound-link").at(0);
+		const history = {push: (h) => {page = h;}};
+		const wrapper = mount(<MemoryRouter>
+			<NotFound history={history}/>
+        </MemoryRouter>);
+        
+        const link = wrapper.find("#notFound-link").at(0);
 		link.simulate("click");
+        
+		asyncCheckCondition(() => {
+            wrapper.update();
+			return page !== null;
+        }, 4000, 100);
+      
+		expect(page).not.toBeNull();
 
-		const redirected = await asyncCheckCondition(
-			() => {return page === "/"},
-			2000,200);
+	});
 
-		wrapper.update();
-		expect(redirected).toBe(true);
-		expect(wrapper.html().includes("Home"));
-    });
+	it("is displayed on invalid routes", () => {
 
-    it("is displayed on invalid routes", () => {
-
-
-    });
+		expect(true).toBe(false);
+	});
 });
